3fe48d4466a8093e442eae7d491097f18845dfc7
Zhanna <kezhanna.03@mail.ru>
2025-05-11 02:04:45 +0500

    added secondHeader

---
+++ components/SecondHeader.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="max-w-[1200px] w-full mx-auto flex justify-between items-center text-[15px]">
+        <img src="../assets/icons/b_1.png" alt="">
+        <p>ВИНО</p>
+        <p>ШАМПАНСКОЕ И ИГРИСТОЕ</p>
+        <p>КРЕПКИЕ НАПИТКИ</p>
+        <p>ЛИКЁРЫ И БИТТЕРЫ</p>
+        <p>FOOD PAIRINGS</p>
+        <p>СЕТЫ</p>
+        <p>СПЕЦИАЛЬНЫЕ ПРЕДЛОЖЕНИЯ</p>
+    </div>
+    <div><hr class="h-[1px] bg-[#D0D0D0] border-0"></div>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,13 @@
 <template>
   <Header></Header>
+  <SecondHeader></SecondHeader>
   <Footer></Footer>
 </template>
 
 <script setup>
 import Footer from '../components/MainFooter.vue';
 import Header from '../components/MainHeader.vue'
+import SecondHeader from '~/components/SecondHeader.vue';
 </script>
 
 <style lang="scss" scoped></style>
