c67d3d29ccc342b0ad7ac3f87949b55b17a7cd70
Zhanna <kezhanna.03@mail.ru>
2025-05-11 17:43:41 +0500

    added img banner

--- components/FirstBody.vue
+++ components/FirstBody.vue
@@ -6,7 +6,9 @@
       <hr class="flex-1 border-0 h-[1px] bg-[#D0D0D0]" />
     </div>
     <div class="w-full flex gap-[15px]">
-      <div class="mb-[20px] max-w-[228px] w-full border-[1px] border-solid border-[#D0D0D0] text-[14px] px-[12px]">
+      <div
+        class="mb-[20px] max-w-[228px] w-full border-[1px] border-solid border-[#D0D0D0] text-[14px] px-[12px]"
+      >
         <div class="flex justify-between items-center gap-[6px]">
           <p class="">
             Вино Chianti Castiglioni, <br />
@@ -25,8 +27,12 @@
         </p>
         <hr class="my-[28px] border-0 h-[1px] bg-[#D0D0D0]" />
         <p class="text-[] text-center mb-[-4px]">34000 тг</p>
-        <div class="my-[6px] max-h-[30px] h-full flex justify-between items-center gap-[4px]">
-          <div class="flex-1 flex justify-between border-[1px] border-sold border-[black] px-[6px] py-[4px]">
+        <div
+          class="my-[6px] max-h-[30px] h-full flex justify-between items-center gap-[4px]"
+        >
+          <div
+            class="flex-1 flex justify-between border-[1px] border-sold border-[black] px-[6px] py-[4px]"
+          >
             <button class="border-none bg-transparent">−</button>
             <span class="">1</span>
             <button class="border-none bg-transparent">+</button>
@@ -38,7 +44,9 @@
           </button>
         </div>
       </div>
-      <div class="mb-[20px] max-w-[228px] w-full border-[1px] border-solid border-[#D0D0D0] text-[14px] px-[12px]">
+      <div
+        class="mb-[20px] max-w-[228px] w-full border-[1px] border-solid border-[#D0D0D0] text-[14px] px-[12px]"
+      >
         <div class="flex justify-between items-center gap-[6px]">
           <p class="">
             Вино Chianti Castiglioni, <br />
@@ -57,8 +65,12 @@
         </p>
         <hr class="my-[28px] border-0 h-[1px] bg-[#D0D0D0]" />
         <p class="text-[] text-center mb-[-4px]">34000 тг</p>
-        <div class="my-[6px] max-h-[30px] h-full flex justify-between items-center gap-[4px]">
-          <div class="flex-1 flex justify-between border-[1px] border-sold border-[black] px-[6px] py-[4px]">
+        <div
+          class="my-[6px] max-h-[30px] h-full flex justify-between items-center gap-[4px]"
+        >
+          <div
+            class="flex-1 flex justify-between border-[1px] border-sold border-[black] px-[6px] py-[4px]"
+          >
             <button class="border-none bg-transparent">−</button>
             <span class="">1</span>
             <button class="border-none bg-transparent">+</button>
@@ -70,7 +82,9 @@
           </button>
         </div>
       </div>
-      <div class="mb-[20px] max-w-[228px] w-full border-[1px] border-solid border-[#D0D0D0] text-[14px] px-[12px]">
+      <div
+        class="mb-[20px] max-w-[228px] w-full border-[1px] border-solid border-[#D0D0D0] text-[14px] px-[12px]"
+      >
         <div class="flex justify-between items-center gap-[6px]">
           <p class="">
             Вино Chianti Castiglioni, <br />
@@ -89,8 +103,12 @@
         </p>
         <hr class="my-[28px] border-0 h-[1px] bg-[#D0D0D0]" />
         <p class="text-[] text-center mb-[-4px]">34000 тг</p>
-        <div class="my-[6px] max-h-[30px] h-full flex justify-between items-center gap-[4px]">
-          <div class="flex-1 flex justify-between border-[1px] border-sold border-[black] px-[6px] py-[4px]">
+        <div
+          class="my-[6px] max-h-[30px] h-full flex justify-between items-center gap-[4px]"
+        >
+          <div
+            class="flex-1 flex justify-between border-[1px] border-sold border-[black] px-[6px] py-[4px]"
+          >
             <button class="border-none bg-transparent">−</button>
             <span class="">1</span>
             <button class="border-none bg-transparent">+</button>
@@ -102,7 +120,9 @@
           </button>
         </div>
       </div>
-      <div class="mb-[20px] max-w-[228px] w-full border-[1px] border-solid border-[#D0D0D0] text-[14px] px-[12px]">
+      <div
+        class="mb-[20px] max-w-[228px] w-full border-[1px] border-solid border-[#D0D0D0] text-[14px] px-[12px]"
+      >
         <div class="flex justify-between items-center gap-[6px]">
           <p class="">
             Вино Chianti Castiglioni, <br />
@@ -121,8 +141,12 @@
         </p>
         <hr class="my-[28px] border-0 h-[1px] bg-[#D0D0D0]" />
         <p class="text-[] text-center mb-[-4px]">34000 тг</p>
-        <div class="my-[6px] max-h-[30px] h-full flex justify-between items-center gap-[4px]">
-          <div class="flex-1 flex justify-between border-[1px] border-sold border-[black] px-[6px] py-[4px]">
+        <div
+          class="my-[6px] max-h-[30px] h-full flex justify-between items-center gap-[4px]"
+        >
+          <div
+            class="flex-1 flex justify-between border-[1px] border-sold border-[black] px-[6px] py-[4px]"
+          >
             <button class="border-none bg-transparent">−</button>
             <span class="">1</span>
             <button class="border-none bg-transparent">+</button>
@@ -134,7 +158,9 @@
           </button>
         </div>
       </div>
-      <div class="mb-[20px] max-w-[228px] w-full border-[1px] border-solid border-[#D0D0D0] text-[14px] px-[12px]">
+      <div
+        class="mb-[20px] max-w-[228px] w-full border-[1px] border-solid border-[#D0D0D0] text-[14px] px-[12px]"
+      >
         <div class="flex justify-between items-center gap-[6px]">
           <p class="">
             Вино Chianti Castiglioni, <br />
@@ -153,8 +179,12 @@
         </p>
         <hr class="my-[28px] border-0 h-[1px] bg-[#D0D0D0]" />
         <p class="text-[] text-center mb-[-4px]">34000 тг</p>
-        <div class="my-[6px] max-h-[30px] h-full flex justify-between items-center gap-[4px]">
-          <div class="flex-1 flex justify-between border-[1px] border-sold border-[black] px-[6px] py-[4px]">
+        <div
+          class="my-[6px] max-h-[30px] h-full flex justify-between items-center gap-[4px]"
+        >
+          <div
+            class="flex-1 flex justify-between border-[1px] border-sold border-[black] px-[6px] py-[4px]"
+          >
             <button class="border-none bg-transparent">−</button>
             <span class="">1</span>
             <button class="border-none bg-transparent">+</button>
@@ -165,10 +195,20 @@
             <img src="../assets/icons/i_14.png" class="w-4 h-4" alt="Bin" />
           </button>
         </div>
-      </div>      
-
+      </div>
     </div>
   </div>
+  <div class="max-w-[1200px] w-full mx-auto my-[20px]">
+    <div class="flex items-center gap-[36px]">
+      <p class="text-[24px]">АКЦИИ</p>
+      <hr class="flex-1 border-0 h-[1px] bg-[#D0D0D0]" />
+    </div>
+    <div class="flex justify-between gap-[20px]">
+      <img src="../assets/icons/i_21.png" alt="" class="w-[590px] h-auto" />
+      <img src="../assets/icons/i_22.png" alt="" class="w-[590px] h-auto" />
+    </div>
+  </div>
+  
 </template>
 
 <script>
