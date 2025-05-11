5d1305fe67fab4e54e8349c9991035585f94654d
Zhanna <kezhanna.03@mail.ru>
2025-05-11 19:04:49 +0500

    add seventh container and img

--- components/FirstBody.vue
+++ components/FirstBody.vue
@@ -484,6 +484,224 @@
       </div>
     </div>
   </div>
+
+  <div class="max-w-[1200px] w-full mx-auto my-[20px]">
+    <div class="flex items-center gap-[36px]">
+      <p class="text-[24px]">ВЫБОР ПОКУПАТЕЛЕЙ</p>
+      <hr class="flex-1 border-0 h-[1px] bg-[#D0D0D0]" />
+    </div>
+    <div class="w-full flex gap-[15px]">
+      <div
+        class="mb-[20px] max-w-[228px] w-full border-[1px] border-solid border-[#D0D0D0] text-[14px] px-[12px]"
+      >
+        <div class="flex justify-between items-center gap-[6px]">
+          <p class="">
+            Вино Chianti Castiglioni, <br />
+            Frescobaldi, 2019 г.
+          </p>
+          <img src="../assets/icons/i_11.png" alt="" />
+        </div>
+        <hr class="flex-1 border-0 h-[1px] bg-[#D0D0D0]" />
+        <div class="flex justify-between items-start">
+          <p class="mt-[-1px]">56955</p>
+          <img src="../assets/icons/i_13.png" alt="" />
+          <img src="../assets/icons/i_12.png" alt="" />
+        </div>
+        <p class="text-center text-[12px] text-[#787878]">
+          Италия, белое сухое, 1л
+        </p>
+        <hr class="my-[28px] border-0 h-[1px] bg-[#D0D0D0]" />
+        <p class="text-[] text-center mb-[-4px]">34000 тг</p>
+        <div
+          class="my-[6px] max-h-[30px] h-full flex justify-between items-center gap-[4px]"
+        >
+          <div
+            class="flex-1 flex justify-between border-[1px] border-sold border-[black] px-[6px] py-[4px]"
+          >
+            <button class="border-none bg-transparent">−</button>
+            <span class="">1</span>
+            <button class="border-none bg-transparent">+</button>
+          </div>
+          <button
+            class="w-1/2 bg-[black] border-none flex justify-center items-center py-[4px]"
+          >
+            <img src="../assets/icons/i_14.png" class="w-4 h-4" alt="Bin" />
+          </button>
+        </div>
+      </div>
+      <div
+        class="mb-[20px] max-w-[228px] w-full border-[1px] border-solid border-[#D0D0D0] text-[14px] px-[12px]"
+      >
+        <div class="flex justify-between items-center gap-[6px]">
+          <p class="">
+            Вино Chianti Castiglioni, <br />
+            Frescobaldi, 2019 г.
+          </p>
+          <img src="../assets/icons/i_11.png" alt="" />
+        </div>
+        <hr class="flex-1 border-0 h-[1px] bg-[#D0D0D0]" />
+        <div class="flex justify-between items-start">
+          <p class="mt-[-1px]">56955</p>
+          <img src="../assets/icons/i_13.png" alt="" />
+          <img src="../assets/icons/i_12.png" alt="" />
+        </div>
+        <p class="text-center text-[12px] text-[#787878]">
+          Италия, белое сухое, 1л
+        </p>
+        <hr class="my-[28px] border-0 h-[1px] bg-[#D0D0D0]" />
+        <p class="text-[] text-center mb-[-4px]">34000 тг</p>
+        <div
+          class="my-[6px] max-h-[30px] h-full flex justify-between items-center gap-[4px]"
+        >
+          <div
+            class="flex-1 flex justify-between border-[1px] border-sold border-[black] px-[6px] py-[4px]"
+          >
+            <button class="border-none bg-transparent">−</button>
+            <span class="">1</span>
+            <button class="border-none bg-transparent">+</button>
+          </div>
+          <button
+            class="w-1/2 bg-[black] border-none flex justify-center items-center py-[4px]"
+          >
+            <img src="../assets/icons/i_14.png" class="w-4 h-4" alt="Bin" />
+          </button>
+        </div>
+      </div>
+      <div
+        class="mb-[20px] max-w-[228px] w-full border-[1px] border-solid border-[#D0D0D0] text-[14px] px-[12px]"
+      >
+        <div class="flex justify-between items-center gap-[6px]">
+          <p class="">
+            Вино Chianti Castiglioni, <br />
+            Frescobaldi, 2019 г.
+          </p>
+          <img src="../assets/icons/i_11.png" alt="" />
+        </div>
+        <hr class="flex-1 border-0 h-[1px] bg-[#D0D0D0]" />
+        <div class="flex justify-between items-start">
+          <p class="mt-[-1px]">56955</p>
+          <img src="../assets/icons/i_13.png" alt="" />
+          <img src="../assets/icons/i_12.png" alt="" />
+        </div>
+        <p class="text-center text-[12px] text-[#787878]">
+          Италия, белое сухое, 1л
+        </p>
+        <hr class="my-[28px] border-0 h-[1px] bg-[#D0D0D0]" />
+        <p class="text-[] text-center mb-[-4px]">34000 тг</p>
+        <div
+          class="my-[6px] max-h-[30px] h-full flex justify-between items-center gap-[4px]"
+        >
+          <div
+            class="flex-1 flex justify-between border-[1px] border-sold border-[black] px-[6px] py-[4px]"
+          >
+            <button class="border-none bg-transparent">−</button>
+            <span class="">1</span>
+            <button class="border-none bg-transparent">+</button>
+          </div>
+          <button
+            class="w-1/2 bg-[black] border-none flex justify-center items-center py-[4px]"
+          >
+            <img src="../assets/icons/i_14.png" class="w-4 h-4" alt="Bin" />
+          </button>
+        </div>
+      </div>
+      <div
+        class="mb-[20px] max-w-[228px] w-full border-[1px] border-solid border-[#D0D0D0] text-[14px] px-[12px]"
+      >
+        <div class="flex justify-between items-center gap-[6px]">
+          <p class="">
+            Вино Chianti Castiglioni, <br />
+            Frescobaldi, 2019 г.
+          </p>
+          <img src="../assets/icons/i_11.png" alt="" />
+        </div>
+        <hr class="flex-1 border-0 h-[1px] bg-[#D0D0D0]" />
+        <div class="flex justify-between items-start">
+          <p class="mt-[-1px]">56955</p>
+          <img src="../assets/icons/i_13.png" alt="" />
+          <img src="../assets/icons/i_12.png" alt="" />
+        </div>
+        <p class="text-center text-[12px] text-[#787878]">
+          Италия, белое сухое, 1л
+        </p>
+        <hr class="my-[28px] border-0 h-[1px] bg-[#D0D0D0]" />
+        <p class="text-[] text-center mb-[-4px]">34000 тг</p>
+        <div
+          class="my-[6px] max-h-[30px] h-full flex justify-between items-center gap-[4px]"
+        >
+          <div
+            class="flex-1 flex justify-between border-[1px] border-sold border-[black] px-[6px] py-[4px]"
+          >
+            <button class="border-none bg-transparent">−</button>
+            <span class="">1</span>
+            <button class="border-none bg-transparent">+</button>
+          </div>
+          <button
+            class="w-1/2 bg-[black] border-none flex justify-center items-center py-[4px]"
+          >
+            <img src="../assets/icons/i_14.png" class="w-4 h-4" alt="Bin" />
+          </button>
+        </div>
+      </div>
+      <div
+        class="mb-[20px] max-w-[228px] w-full border-[1px] border-solid border-[#D0D0D0] text-[14px] px-[12px]"
+      >
+        <div class="flex justify-between items-center gap-[6px]">
+          <p class="">
+            Вино Chianti Castiglioni, <br />
+            Frescobaldi, 2019 г.
+          </p>
+          <img src="../assets/icons/i_11.png" alt="" />
+        </div>
+        <hr class="flex-1 border-0 h-[1px] bg-[#D0D0D0]" />
+        <div class="flex justify-between items-start">
+          <p class="mt-[-1px]">56955</p>
+          <img src="../assets/icons/i_13.png" alt="" />
+          <img src="../assets/icons/i_12.png" alt="" />
+        </div>
+        <p class="text-center text-[12px] text-[#787878]">
+          Италия, белое сухое, 1л
+        </p>
+        <hr class="my-[28px] border-0 h-[1px] bg-[#D0D0D0]" />
+        <p class="text-[] text-center mb-[-4px]">34000 тг</p>
+        <div
+          class="my-[6px] max-h-[30px] h-full flex justify-between items-center gap-[4px]"
+        >
+          <div
+            class="flex-1 flex justify-between border-[1px] border-sold border-[black] px-[6px] py-[4px]"
+          >
+            <button class="border-none bg-transparent">−</button>
+            <span class="">1</span>
+            <button class="border-none bg-transparent">+</button>
+          </div>
+          <button
+            class="w-1/2 bg-[black] border-none flex justify-center items-center py-[4px]"
+          >
+            <img src="../assets/icons/i_14.png" class="w-4 h-4" alt="Bin" />
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="max-w-[1200px] w-full mx-auto my-[20px]">
+    <div class="flex items-center gap-[36px]">
+      <p class="text-[24px]">БРЕНДЫ</p>
+      <hr class="flex-1 border-0 h-[0px] bg-[#D0D0D0]" />
+    </div>
+    <div class="grid grid-cols-5 place-items-center gap-[50px]">
+        <img src="../assets/icons/i_70.svg" alt="">
+        <img src="../assets/icons/i_72.svg" alt="">
+        <img src="../assets/icons/i_73.svg" alt="">
+        <img src="../assets/icons/i_74.svg" alt="">
+        <img src="../assets/icons/i_75.svg" alt="">
+        <img src="../assets/icons/i_76.svg" alt="">
+        <img src="../assets/icons/i_77.svg" alt="">
+        <img src="../assets/icons/i_78.svg" alt="">
+        <img src="../assets/icons/i_79.svg" alt="">
+        <img src="../assets/icons/i_80.svg" alt="">
+    </div>
+  </div>
 </template>
 
 <script>
