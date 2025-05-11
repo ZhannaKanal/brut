7f6869ed74f7892efae1ad674b2a795079c4a5bf
Zhanna <kezhanna.03@mail.ru>
2025-05-11 19:17:16 +0500

    added 8th container

--- components/FirstBody.vue
+++ components/FirstBody.vue
@@ -472,14 +472,22 @@
       <hr class="flex-1 border-0 h-[1px] bg-[#D0D0D0]" />
     </div>
     <div class="flex justify-between gap-[20px] text-[white] text-[24px]">
-      <div class="relative  w-[590px]">
+      <div class="relative w-[590px]">
         <img src="../assets/icons/i_51.png" alt="" class="w-full h-auto" />
-        <img src="../assets/icons/i_53.png" alt="" class="absolute top-[210px] left-[10px]" />
+        <img
+          src="../assets/icons/i_53.png"
+          alt=""
+          class="absolute top-[210px] left-[10px]"
+        />
         <p class="absolute bottom-[0] left-[165px]">Персонализация бутылок</p>
       </div>
       <div class="relative">
         <img src="../assets/icons/i_52.png" alt="" class="w-[590px] h-auto" />
-        <img src="../assets/icons/i_54.png" alt="" class="absolute top-[210px] right-[10px]" />
+        <img
+          src="../assets/icons/i_54.png"
+          alt=""
+          class="absolute top-[210px] right-[10px]"
+        />
         <p class="absolute bottom-[10px] left-[195px]">Подарочные наборы</p>
       </div>
     </div>
@@ -690,17 +698,36 @@
       <hr class="flex-1 border-0 h-[0px] bg-[#D0D0D0]" />
     </div>
     <div class="grid grid-cols-5 place-items-center gap-[50px]">
-        <img src="../assets/icons/i_70.svg" alt="">
-        <img src="../assets/icons/i_72.svg" alt="">
-        <img src="../assets/icons/i_73.svg" alt="">
-        <img src="../assets/icons/i_74.svg" alt="">
-        <img src="../assets/icons/i_75.svg" alt="">
-        <img src="../assets/icons/i_76.svg" alt="">
-        <img src="../assets/icons/i_77.svg" alt="">
-        <img src="../assets/icons/i_78.svg" alt="">
-        <img src="../assets/icons/i_79.svg" alt="">
-        <img src="../assets/icons/i_80.svg" alt="">
+      <img src="../assets/icons/i_70.svg" alt="" />
+      <img src="../assets/icons/i_72.svg" alt="" />
+      <img src="../assets/icons/i_73.svg" alt="" />
+      <img src="../assets/icons/i_74.svg" alt="" />
+      <img src="../assets/icons/i_75.svg" alt="" />
+      <img src="../assets/icons/i_76.svg" alt="" />
+      <img src="../assets/icons/i_77.svg" alt="" />
+      <img src="../assets/icons/i_78.svg" alt="" />
+      <img src="../assets/icons/i_79.svg" alt="" />
+      <img src="../assets/icons/i_80.svg" alt="" />
     </div>
+  </div>
+
+  <div
+    class="max-w-[1200px] w-full mx-auto my-[70px] pb-[50px] border-[0.5px] border-[#00000040]"
+  >
+    <img class="pl-[20px] pt-[20px]" src="../assets/icons/i_81.svg" alt="" />
+    <p class="px-[50px] my-[0] leading-[30px]">
+      Алкогольная продукция, представленная на сайте http://www.brut.kz/ может
+      быть приобретена только в пункте выдачи или в одной из винотек, <br />
+      расположенных в городах Алматы, Нур-Султан и Караганды. Розничная продажа
+      осуществляется на основании лицензий на розничную продажу алкогольной
+      продукции. <br />
+      Адреса местонахождений торговых объектов, время их работы, а также иную
+      информацию вы можете посмотреть в разделе Наши винотеки. <br />
+      Мы не осуществляем доставку алкогольной продукции. Запрет на дистанционную
+      продажу алкогольной продукции установлен <br />
+      Федеральным законом от 22 ноября 1995 г. № 171-ФЗ и постановлением
+      Республики Казахстан от 27 сентября 2007 г. № 612.
+    </p>
   </div>
 </template>
 
